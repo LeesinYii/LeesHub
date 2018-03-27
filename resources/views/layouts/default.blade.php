f0c7f3412a1f1565a30665383ba2711dff0b90d0
leesinyii <Leesin_code@163.com>
2018-03-27 22:30:59 +0800

    u

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,17 @@
 <head>
 	<meta charset="UTF-8">
 	<title>LeesHub|@yield('title','首页') - 基于Laravel的开源社区</title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-	@yield('content')
+	@include('layouts._header')
+
+   	<div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div> 
+   
 </body>
 </html>
